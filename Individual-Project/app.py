1ac86deac14a998787c0987e2f1c2dc83dfa5ce7
Hasanhamdan789 <HasanHamdan789@gmail.com>
2023-07-26 15:16:19 +0300

    almost done

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -21,46 +21,75 @@ auth = firebase.auth()
 db = firebase.database()
 
 
-#Code goes below here
 @app.route('/', methods=['GET', 'POST'])
 def signup():
-    error = ""
     if request.method == 'POST':
         email = request.form['email']
-        first_name = request.form['first_name']
         password = request.form['password']
+        first_name = request.form['first_name']
         last_name = request.form['last_name']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
             UID = login_session['user']['localId']
             user = {"first_name": first_name, "email": email, "password": password, "last_name": last_name}
             db.child("Users").child(UID).set(user)
-            return redirect(url_for("home"))
+            flash("Account created successfully! Please log in.", "success")
+            return redirect(url_for("login"))
         except Exception as e:
             print(e)
-            error = "Signup Failed"
+            flash("Signup Failed. Please try again.", "error")
     return render_template("index.html")
 
+
 @app.route('/login', methods=['GET', 'POST'])
-def login():  
-    error = ""
+def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         try:
             login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return redirect(url('home'))
+            user_id = login_session['user']['localId']
+            user_data = db.child("Users").child(user_id).get().val()
+            login_session['user_data'] = user_data
+            return redirect(url_for('home'))
         except:
-            error = "Authentication Failed"
+            flash("Authentication Failed. Please check your credentials.", "error")
     return render_template("login.html")
+
+
+@app.route('/logout')
+def logout():
+    login_session.clear()
+    return redirect(url_for('login'))
+
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if 'user' not in login_session:
+        return redirect(url_for('login'))
 
+    user_id = login_session['user']['localId']
 
+    if request.method == 'POST':
+        if request.form['submit'] == 'add':
+            try:
+                name = request.form['name']
+                db.child("Users").child(user_id).child("items").push(name)
+                user_data = db.child("Users").child(user_id).get()
+                to = user_data.val() if user_data else []
+                return render_template('home.html', t=to.values())
+            except:
+                flash("Error adding item. Please try again.", "error")
+        elif request.form['submit'] == 'delete':
+            db.child("Users").child(user_id).child("items").remove()
+            login_session['user_data'] = db.child("Users").child(user_id).get().val()
+            flash("All items deleted.", "success")
+        return render_template('home.html')
 
-#Code goes above here
+    user_data = db.child("Users").child(user_id).get()
+    to = user_data.val() if user_data else []
+    return render_template('home.html', t=to.values())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
